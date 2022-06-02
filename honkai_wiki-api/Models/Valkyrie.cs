99075b434280cb6c6302f53824be19b845df569c
tehtko <moderndsnsam@gmail.com>
2022-06-01 20:06:19 -0400

    Update Model

--- honkai_wiki-api/Models/Valkyrie.cs
+++ honkai_wiki-api/Models/Valkyrie.cs
@@ -2,5 +2,10 @@
 {
     public class Valkyrie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Weapon { get; set; }
+        public string Description { get; set; }
     }
 }
