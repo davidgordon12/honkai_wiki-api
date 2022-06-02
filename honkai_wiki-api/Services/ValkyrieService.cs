bd01f3f34048e059317af66984676174880b6430
tehtko <moderndsnsam@gmail.com>
2022-06-01 20:13:53 -0400

    Create GetMethod using SqlDataClient

--- honkai_wiki-api/Services/ValkyrieService.cs
+++ honkai_wiki-api/Services/ValkyrieService.cs
@@ -1,4 +1,5 @@
 ﻿using honkai_wiki_api.Data;
+using honkai_wiki_api.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 
@@ -11,13 +12,32 @@ namespace honkai_wiki_api.Services
         public async Task<JsonResult> GetValkyriesAsync()
         {
             HonkaiContext context = new HonkaiContext();
+            List<Valkyrie> valkyries = new();
 
             command = new SqlCommand("SELECT * FROM Valkyries", context.sqlConnection);
 
             using (var ctx = context.sqlConnection)
             {
-                var res = await Task.Run(() => ctx);
-                return new JsonResult(res);
+                ctx.Open();
+                var task = await Task.Run(async () =>
+                    reader = await command.ExecuteReaderAsync()
+                );
+
+                while(reader.Read())
+                {
+                    valkyries.Add(new Valkyrie
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Image = reader.GetString(2),
+                        Weapon = reader.GetInt32(3),
+                        Description = reader.GetString(4)
+                    });
+                }
+
+                ctx.Close();
+
+                return new JsonResult(valkyries);
             }
         }
     }
