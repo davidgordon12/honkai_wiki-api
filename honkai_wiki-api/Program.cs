44a4a5ed6665191e68efbfa846532a5f4264b920
tehtko <moderndsnsam@gmail.com>
2022-06-08 20:14:25 -0400

    Add CORS middleware

--- honkai_wiki-api/Program.cs
+++ honkai_wiki-api/Program.cs
@@ -1,10 +1,19 @@
+var origin = "_origin";
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 honkai_wiki_api.Data.HonkaiContext.conString = builder.Configuration.GetConnectionString("Akali");
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: origin,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:44304");
+                      });
+});
 
 var app = builder.Build();
 
@@ -17,18 +26,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(origin);
 app.UseHttpsRedirection();
 
 app.MapGet("/valkyries", async () =>
 {
     var valkyries = await valkyrieService.GetValkyriesAsync();
-    return valkyries;
+    return Results.Ok(valkyries);
 });
 
 app.MapGet("/valkyrie/{id}", async (int id) =>
 {
     var valkyrie = await valkyrieService.GetValkyrieAsync(id);
-    return valkyrie;
+    return Results.Ok(valkyrie);
 });
 
 app.Run();
