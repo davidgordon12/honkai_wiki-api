4df9ff66db887ae1677003694ade9fcae30efe1e
tehtko <moderndsnsam@gmail.com>
2022-06-15 15:41:31 -0400

    Update Weapon Model

--- honkai_wiki-api/Models/Weapon.cs
+++ honkai_wiki-api/Models/Weapon.cs
@@ -2,5 +2,12 @@
 {
     public class Weapon
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+        public float Atk { get; set; }
+        public float Crt { get; set; }
     }
 }
