6c90b41778dda8460ce445501c3005e68776c1c1
tehtko <moderndsnsam@gmail.com>
2022-06-15 15:42:14 -0400

    Update Stigmata Model

--- honkai_wiki-api/Models/Stigmata.cs
+++ honkai_wiki-api/Models/Stigmata.cs
@@ -2,5 +2,10 @@
 {
     public class Stigmata
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
     }
 }
