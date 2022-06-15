e8f4956404a303cae0b2d8072e5e3bdf61afab43
tehtko <moderndsnsam@gmail.com>
2022-06-15 15:32:47 -0400

    Update Battlesuit model

--- honkai_wiki-api/Models/Battlesuit.cs
+++ honkai_wiki-api/Models/Battlesuit.cs
@@ -2,5 +2,12 @@
 {
     public class Battlesuit
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Type { get; set; }
+        public int Weapon { get; set; }
+        public int Valkyrie { get; set; }
     }
 }
