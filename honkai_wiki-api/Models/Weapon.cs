7796237416cb42f72d3291fc423f553623ebc215
tehtko <moderndsnsam@gmail.com>
2022-06-19 20:48:53 -0400

    Remove Description column

--- honkai_wiki-api/Models/Weapon.cs
+++ honkai_wiki-api/Models/Weapon.cs
@@ -1,10 +1,9 @@
-﻿namespace honkai_wiki_api.Models
+namespace honkai_wiki_api.Models
 {
     public class Weapon
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public string Image { get; set; }
         public string Type { get; set; }
         public float Atk { get; set; }
