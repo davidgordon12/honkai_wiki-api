3348697b7ee22cc9b1fec5db05badf4144a96e05
tehtko <moderndsnsam@gmail.com>
2022-06-22 20:55:44 -0400

    Update model

--- honkai_wiki-api/Models/Battlesuit.cs
+++ honkai_wiki-api/Models/Battlesuit.cs
@@ -6,6 +6,8 @@ namespace honkai_wiki_api.Models
         public string Name { get; set; }
         public string Image { get; set; }
         public string Type { get; set; }
+        public string Element { get; set; }
+        public string Rank { get; set; }
         public int Weapon { get; set; }
         public int Valkyrie { get; set; }
     }
